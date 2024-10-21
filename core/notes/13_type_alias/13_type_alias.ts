0eb1f8de474376b160a817d0acd4108a6956eb1f
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-10-21 15:34:27 -0300

    feat: Estudo sobre `type alias`

---
+++ core/notes/13_type_alias/13_type_alias.ts
@@ -0,0 +1,15 @@
+type Idade = number;
+type Pessoa = {
+    nome: string;
+    idade: Idade;
+    salario: number;
+    corPreferida?: string;
+};
+
+type CorRGB = 'red' | 'green' | 'blue';
+type CorCMYK = 'Ciano' | 'Magenta' | 'Amarelo' | 'Preto';
+type CorPreferida = CorRGB | CorCMYK;
+
+function setCorPreferida(pessoa: Pessoa, cor: CorPreferida): Pessoa{
+    return {...pessoa, corPreferida: cor}
+}
