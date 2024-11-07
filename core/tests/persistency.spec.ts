9bd4c90ddaa4e4505428a9648ffbe3f768f74d55
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-11-07 20:15:58 -0300

    feat: Estudo sobre `spyOn`

--- core/tests/persistency.spec.ts
+++ core/tests/persistency.spec.ts
@@ -1,6 +1,25 @@
-describe('Persistência:', () => {
-    it('Escrevendo dados.', () => {
-        const writeData = true;
-        expect(writeData).toBe(true);
+import { Persistency } from "../solid/2-single-responsibility-principle/services/persistency";
+
+describe('Persistency', () => {
+    afterEach(() => jest.clearAllMocks());
+
+    it('Deve retorna indefinido', () => {
+        // System under test
+        const sut = new Persistency();
+        expect(sut.saveOrder()).toBeUndefined();
+    })
+
+    it('Deve chamar console.log uma vez', () => {
+        const sut = new Persistency();
+        const consoleSpy = jest.spyOn(console, 'log');
+        sut.saveOrder();
+        expect(consoleSpy).toHaveBeenCalledTimes(1);
+    })
+
+    it('Deve chamar console.log com "Pedido salvo com sucesso..."', () => {
+        const sut = new Persistency();
+        const consoleSpy = jest.spyOn(console, 'log');
+        sut.saveOrder();
+        expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...')
     })
 })
